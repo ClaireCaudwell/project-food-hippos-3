386c4e798a022e810a55640d3a09f288a90706d2
annahasselstrom <64977313+annahasselstrom@users.noreply.github.com>
2020-09-30 12:05:55 +0200

    Added html.

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const cityId = 279; // Seattle
 const categoryId = 8; // Breakfast
 const API_KEY = '3b26b8095534eabb0168c26dedd59660';
-//const requestURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&category=${categoryId}`;
 const requestURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&count=10&category=${categoryId}`;
-
-//const requestURL2 = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&cuisines=${cuisineId}`;
 
 const request = new Request(requestURL, {
     headers: new Headers({
@@ -19,14 +16,41 @@ const request = new Request(requestURL, {
     .then(json => {
 
     console.log(json); // prints array to console
-    //json.restaurants.forEach(data => { // loops over each item in the array.
-      //console.log(data.restaurant.name); // returns and prints the array's names to the console.
-      
+      //json.restaurants.forEach(data => { // loops over each item in the array.      
       json.restaurants.forEach((data) => {
         restaurantAddress.innerHTML += generateHTMLForForecast(data);
       });
     });
-      /*document.getElementById("restaurant-list").innerHTML += //fetches, returns and outputs specified features from the API in html.
+      
+  const generateHTMLForForecast = data => {
+    const restName = data.restaurant.name;
+    const restAddress = data.restaurant.location.address;
+    const restImage = data.restaurant.featured_image;
+    const rating = data.restaurant.user_rating.aggregate_rating;
+    const averageCost = data.restaurant.average_cost_for_two / 2;
+ 
+    // And creates HTML code that is returned
+    let createRestaurantHTML = '';
+    createRestaurantHTML += `<div class="restaurant-container">`;
+    createRestaurantHTML += `<p id="restaurantName">${restName}</p>`;
+    createRestaurantHTML += `<p class="rest-address">${restAddress}</p>`;
+    createRestaurantHTML += `<img src="${restImage}" id="restaurantImage="rest-img">`;
+    createRestaurantHTML += `<p id="rating">${rating}</p>`;
+    createRestaurantHTML += `<p id="averageCost">${averageCost}</p>`;
+    createRestaurantHTML += `</div>`;
+    return createRestaurantHTML
+  };
+  
+  /*const getsPlaceholderImage = () => {
+    const featuredImage = data.restaurant.featured_image;
+    if (featuredImage) 
+    return featuredImage 
+   } else 
+    return (url) // subtitute image
+    
+  } */
+
+  /*document.getElementById("restaurant-list").innerHTML += //fetches, returns and outputs specified features from the API in html.
       `<li>
 
       ${data.restaurant.name}
@@ -44,36 +68,3 @@ const request = new Request(requestURL, {
       })" </div>`;
 */
     
-
-
-  const generateHTMLForForecast = data => {
-    const restName = data.restaurant.name;
-    const restAddress = data.restaurant.location.address;
-    const restImage = data.restaurant.featured_image;
-    const rating = data.restaurant.user_rating.aggregate_rating;
-    const averageCost = data.restaurant.average_cost_for_two / 2;
- 
-
-    // And creates HTML code that is returned
-    let createRestaurantHTML = '';
-    createRestaurantHTML += `<div class="restaurant-container">`;
-    createRestaurantHTML += `<p>${restName}</p>`;
-    createRestaurantHTML += `<p>${restAddress}</p>`;
-    createRestaurantHTML += `<img src="${restImage}">`;
-    createRestaurantHTML += `<p>${rating}</p>`;
-    createRestaurantHTML += `<p>${averageCost}</p>`;
-    createRestaurantHTML += `</div>`;
-    return createRestaurantHTML
-  };
-  
-
-  //console.log()
-
-  /*const getsPlaceholderImage = () => {
-    const featuredImage = data.restaurant.featured_image;
-    if (featuredImage) 
-    return featuredImage 
-   } else 
-    return (url) // subtitute image
-    
-  } */

17341ee3c403760b10f53c262a1e2a1f4751d1da
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-01 21:42:02 +0200

    Updated HTML, CSS & JS files 1. Updated Site name to Hangry? 2. Updated some class names with kebab-case method. 3. Deleted best restaurant section as not using. 4. Added a main tag to contain the filter and restaurant list sections. 5. Deleted commented out restaurant card section as now created in js. 6. In JS changed the count from 10 to 12 so we could have three cards fit on tablet and desktop views. 7. Updated the classes and id's according to the changes made in HTML and so they follow camelCase and Kebab case. 8. Deleted restaurant-container div as wea already have this declared in JS further above. Replaced it with restaurant-cardsto create the div for the cards. 9. Added a restaurant-details div so we can add a background colour of white to the details of the restaurant in the card. 10. Added additional text details in each line of restaurant details. 11. In CCS updated the class names inline with the changes made in HTML and JS. 12. Added styling to the font awesome magnify glass icon. 13. Added some styling for the main area so the cards would be centered. 14. Added styling for restaurant details. 15. Gave the rest-name and rest-address a height so they would be inline horitontally as the restaurant name and address are different lengths. 16. Made the rest-img object-fit: cover so all of the images would be the same size. 17. Updated the media queries.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const cityId = 279; // Seattle
 const categoryId = 8; // Breakfast
 const API_KEY = '3b26b8095534eabb0168c26dedd59660';
-const requestURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&count=10&category=${categoryId}`;
+const requestURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&count=12&category=${categoryId}`;
 
 const request = new Request(requestURL, {
     headers: new Headers({
@@ -11,7 +11,7 @@ const request = new Request(requestURL, {
   });
 // Added new variable const containerRestaurant.
 // Changed restaurantAddress to containerRestaurant in the forEach.
-  const containerRestaurant = document.getElementById('restaurant-container');
+  const containerRestaurant = document.getElementById('restaurantContainer');
 
 // Using a new request to fetch data. The new request includes API-key (user-key) and url (requestURL).
   fetch(request) 
@@ -33,13 +33,15 @@ const request = new Request(requestURL, {
     const averageCost = data.restaurant.average_cost_for_two / 2;
  
     // And creates HTML code that is returned
-    let createRestaurantHTML = '';
-    createRestaurantHTML += `<div class="restaurant-container">`;
+    let createRestaurantHTML = '';    
+    createRestaurantHTML += `<div class="restaurant-cards" id="restaurant-list">`;
+    createRestaurantHTML += `<div class="restaurant-details">`;
     createRestaurantHTML += `<p class="rest-name" id="restaurantName">${restName}</p>`;
-    createRestaurantHTML += `<p class="rest-address">${restAddress}</p>`;
+    createRestaurantHTML += `<p class="rest-address"><b>Address:</b> ${restAddress}</p>`;
     createRestaurantHTML += `<img src="${restImage}" class="rest-img">`;
-    createRestaurantHTML += `<p class="rest-rating" id="rating">${rating}</p>`;
-    createRestaurantHTML += `<p class="ave-cost" id="averageCost">${averageCost}</p>`;
+    createRestaurantHTML += `<p class="rest-rating" id="rating"><b>Average rating:</b> ${rating} &#9733;&#9733;</p>`;
+    createRestaurantHTML += `<p class="ave-cost" id="averageCost"><b>Average cost:</b> ${averageCost}$</p>`;
+    createRestaurantHTML += `</div>`;
     createRestaurantHTML += `</div>`;
     return createRestaurantHTML
   };

9da8eeebdc9bfe9934e902a868cf1c9c4083a96c
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-02 16:36:28 +0200

    Updated HTML, CSS & JS files: 1. Added some radios buttons to try this out. Have commented these out for now, but maybe something to refer to fo the furture. 2. Added a button to sort the restaurants by rating and also to reload the page back to it's original state so the user can sort again by rating if they want. 3. CSS: Added radion button styling, which we've commented out for now if we need to use in the future. 4. Added styling for buttons. 5. Updated the colours with RGB so we don't have mixed colour types e.g. hex, rgb etc. 6. JS file: Added the url for the restuarants sorted by rating that the api does for us. 7. Put the basic fetch, which fetches the list of 12 restaurants, into it's own function called basicRequest. We did this so we could create a section function called sortedRequest. 8. SortedRequest function holds the new request from the second URL, the fetch for that restaurant info that's sorted by rating. Within this function we set the containerRestaurant to an empty string and re-insert the already created html into there replacing it with the information we got from the second fetch i.e. restuarants that have been sorted by their rating. 9. Also created the addEventListener so when we click on the button topRatings in HTML it will call the sortedRequest function. 8. Added a reload function, so when we click on the clear search button this function is called and the page is reloaded.

--- script.js
+++ script.js
@@ -3,73 +3,83 @@ const categoryId = 8; // Breakfast
 const API_KEY = '3b26b8095534eabb0168c26dedd59660';
 const requestURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&count=12&category=${categoryId}`;
 
+const requestURLRating = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&count=12&category=${categoryId}&sort=rating`;
+
 const request = new Request(requestURL, {
-    headers: new Headers({
-      'Accept': 'application/json',
-      'user-key': `${API_KEY}` 
-    })
-  });
+  headers: new Headers({
+    'Accept': 'application/json',
+    'user-key': `${API_KEY}` 
+  })
+});
+
 // Added new variable const containerRestaurant.
 // Changed restaurantAddress to containerRestaurant in the forEach.
-  const containerRestaurant = document.getElementById('restaurantContainer');
+const containerRestaurant = document.getElementById('restaurantContainer');
 
 // Using a new request to fetch data. The new request includes API-key (user-key) and url (requestURL).
+const basicRequest = () => {
   fetch(request) 
-    .then(response => response.json())
-    .then(json => {
+  .then(response => response.json())
+  .then(json => {
 
-    console.log(json); // prints array to console
-      //json.restaurants.forEach(data => { // loops over each item in the array.      
-      json.restaurants.forEach((data) => {
-        containerRestaurant.innerHTML += generateHTMLForForecast(data);
-      });
+  console.log(json); // prints array to console
+    //json.restaurants.forEach(data => { // loops over each item in the array.      
+    json.restaurants.forEach((data) => {
+      containerRestaurant.innerHTML += generateHTMLForRestaurants(data);
     });
+  });
+};
+
+basicRequest();
+// console.log(basicRequest);
+
+const requestTwo = new Request(requestURLRating, {
+  headers: new Headers({
+    'Accept': 'application/json',
+    'user-key': `${API_KEY}` 
+  })
+});
+
+const sortedRequest = () => {
+  fetch(requestTwo) 
+  .then(response => response.json())
+  .then(json => {
+    containerRestaurant.innerHTML = "";
+    //json.restaurants.forEach(data => { // loops over each item in the array.      
+    json.restaurants.forEach((data) => {
+      containerRestaurant.innerHTML += generateHTMLForRestaurants(data);
+    });
+  });
+};
+
+document.getElementById("topRatings").addEventListener("click", sortedRequest); 
       
-  const generateHTMLForForecast = data => {
-    const restName = data.restaurant.name;
-    const restAddress = data.restaurant.location.address;
-    const restImage = data.restaurant.featured_image;
-    const rating = data.restaurant.user_rating.aggregate_rating;
-    const averageCost = data.restaurant.average_cost_for_two / 2;
- 
-    // And creates HTML code that is returned
-    let createRestaurantHTML = '';    
-    createRestaurantHTML += `<div class="restaurant-cards" id="restaurant-list">`;
-    createRestaurantHTML += `<div class="restaurant-details">`;
-    createRestaurantHTML += `<p class="rest-name" id="restaurantName">${restName}</p>`;
-    createRestaurantHTML += `<p class="rest-address"><b>Address:</b> ${restAddress}</p>`;
-    createRestaurantHTML += `<img src="${restImage}" class="rest-img">`;
-    createRestaurantHTML += `<p class="rest-rating" id="rating"><b>Average rating:</b> ${rating} &#9733;&#9733;</p>`;
-    createRestaurantHTML += `<p class="ave-cost" id="averageCost"><b>Average cost:</b> ${averageCost}$</p>`;
-    createRestaurantHTML += `</div>`;
-    createRestaurantHTML += `</div>`;
-    return createRestaurantHTML
-  };
-  
-  /*const getsPlaceholderImage = () => {
-    const featuredImage = data.restaurant.featured_image;
-    if (featuredImage) 
-    return featuredImage 
-   } else 
-    return (url) // subtitute image
-    
-  } */
+const generateHTMLForRestaurants = data => {
+  const restName = data.restaurant.name;
+  const restAddress = data.restaurant.location.address;
+  const restImage = data.restaurant.featured_image;
+  const rating = data.restaurant.user_rating.aggregate_rating;
+  const averageCost = data.restaurant.average_cost_for_two / 2;
 
-  /*document.getElementById("restaurant-list").innerHTML += //fetches, returns and outputs specified features from the API in html.
-      `<li>
+  // And creates HTML code that is returned
+  let createRestaurantHTML = '';    
+  createRestaurantHTML += `<div class="restaurant-cards" id="restaurant-list">`;
+  createRestaurantHTML += `<div class="restaurant-details">`;
+  createRestaurantHTML += `<p class="rest-name" id="restaurantName">${restName}</p>`;
+  createRestaurantHTML += `<p class="rest-address"><b>Address:</b> ${restAddress}</p>`;
+  createRestaurantHTML += `<img src="${restImage}" class="rest-img">`;
+  createRestaurantHTML += `<p class="rest-rating" id="rating"><b>Average rating:</b> ${rating} &#9733;&#9733;</p>`;
+  createRestaurantHTML += `<p class="ave-cost" id="averageCost"><b>Average cost:</b> ${averageCost}$</p>`;
+  createRestaurantHTML += `</div>`;
+  createRestaurantHTML += `</div>`;
+  return createRestaurantHTML
+};
 
-      ${data.restaurant.name}
-      ${data.restaurant.location.address} 
-      ${data.restaurant.cuisines} 
-      ${data.restaurant.establishment} 
-      ${data.restaurant.user_rating.aggregate_rating}
-      ${data.restaurant.average_cost_for_two}
-      ${data.restaurant.average_cost_for_two / 2}
-      ${data.restaurant.price_range} 
-      ${data.restaurant.featured_image}
+// const restart = () => { 
+//   document.location.href = ""; 
+// };
 
-      </li> 
-      <div style="width: 100px; height: 100px; 
-      })" </div>`;
-*/
+const reloadButton = () => { 
+  location.reload(); 
+};
     
